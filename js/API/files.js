2041d38b80a476451c7aac9148c2f5df7901d571
Miguel Alvarez <miguelazpeitia@yahoo.com>
2013-01-18 20:44:10 -0600

    test

--- js/API/files.js
+++ js/API/files.js
@@ -21,9 +21,14 @@ $(document).ready(function(e) {
            
         };
         writer.write($("#archivosCamp").val());
-    }
+    
+	
+	
+	}
 
-
+			function fail(err){
+				alert("error: "+err.code);
+				}
 										
 					break;
 				case 1://Leer Archivo
